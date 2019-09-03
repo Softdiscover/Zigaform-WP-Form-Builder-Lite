0afdf27919afb512e99a5ed32289b154c953839f
laranginha <metroanuncios@gmail.com>
2019-09-03 00:34:34 -0500

    Version 3.9.9.8.5

--- modules/formbuilder/controllers/uiform-fb-controller-forms.php
+++ modules/formbuilder/controllers/uiform-fb-controller-forms.php
@@ -425,10 +425,10 @@ class Uiform_Fb_Controller_Forms extends Uiform_Base_Module {
         $dump_form = unserialize(Uiform_Form_Helper::base64url_decode($imp_form));
         $data_form = array();
         $data_form['fmb_data'] = json_decode($dump_form['fmb_data']);
-        $data_form['fmb_html_backend'] = $dump_form['fmb_html_backend'];
-        $data_form['fmb_name'] = $dump_form['fmb_name'];
-        $data_form['fmb_rec_tpl_html'] = $dump_form['fmb_rec_tpl_html'];
-        $data_form['fmb_rec_tpl_st'] = $dump_form['fmb_rec_tpl_st'];
+        $data_form['fmb_html_backend'] = @$dump_form['fmb_html_backend'];
+        $data_form['fmb_name'] = @$dump_form['fmb_name'];
+        $data_form['fmb_rec_tpl_html'] = @$dump_form['fmb_rec_tpl_html'];
+        $data_form['fmb_rec_tpl_st'] = @$dump_form['fmb_rec_tpl_st'];
         $json = array();
         $json['data'] = $data_form;
         header('Content-Type: application/json');
--- modules/formbuilder/controllers/uiform-fb-controller-frontend.php
+++ modules/formbuilder/controllers/uiform-fb-controller-frontend.php
@@ -1578,7 +1578,7 @@ class Uiform_Fb_Controller_Frontend extends Uiform_Base_Module {
         wp_enqueue_style('rockfm-bootstrap-datetimepicker', UIFORM_FORMS_URL . '/assets/backend/js/bdatetime/bootstrap-datetimepicker.css');
         
         // bootstrap datetimepicker2
-        wp_enqueue_style('rockfm-bootstrap-datetimepicker2', UIFORM_FORMS_URL . '/assets/common/js/flatpickr/4.5.2/flatpickr.min.css');
+        wp_enqueue_style('rockfm-bootstrap-datetimepicker2', UIFORM_FORMS_URL . '/assets/common/js/flatpickr/4.6.2/flatpickr.min.css');
         
         //color picker
         wp_enqueue_style('rockfm-bootstrap-colorpicker', UIFORM_FORMS_URL . '/assets/backend/js/colorpicker/2.5/css/bootstrap-colorpicker.css');
@@ -1637,8 +1637,8 @@ class Uiform_Fb_Controller_Frontend extends Uiform_Base_Module {
         wp_enqueue_script('rockfm-bootstrap-datetimepicker', UIFORM_FORMS_URL . '/assets/backend/js/bdatetime/bootstrap-datetimepicker.js', array('jquery', 'rockfm-bootstrap'), '1.0', true);
         
          //bootstrap datetimepicker
-        wp_enqueue_script('rockfm-bootstrap-dtpicker-locales2', UIFORM_FORMS_URL . '/assets/common/js/flatpickr/4.5.2/flatpickr.js', array('jquery', 'rockfm-bootstrap'), '1.0', true);
-        wp_enqueue_script('rockfm-bootstrap-datetimepicker2', UIFORM_FORMS_URL . '/assets/common/js/flatpickr/4.5.2/l10n/all-lang.js', array('jquery', 'rockfm-bootstrap'), '1.0', true);
+        wp_enqueue_script('rockfm-bootstrap-dtpicker-locales2', UIFORM_FORMS_URL . '/assets/common/js/flatpickr/4.6.2/flatpickr.js', array('jquery', 'rockfm-bootstrap'), '1.0', true);
+        wp_enqueue_script('rockfm-bootstrap-datetimepicker2', UIFORM_FORMS_URL . '/assets/common/js/flatpickr/4.6.2/l10n/all-lang.js', array('jquery', 'rockfm-bootstrap'), '1.0', true);
         
         //star rating
         wp_enqueue_script('rockfm-star-rating', UIFORM_FORMS_URL . '/assets/backend/js/bratestar/star-rating.js', array('jquery', 'rockfm-bootstrap'), '1.0', true);
