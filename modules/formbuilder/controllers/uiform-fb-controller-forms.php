34e46b542c9c5ff35001aa5b4b2b25853c25a734
laranginha <metroanuncios@gmail.com>
2019-10-14 15:17:58 -0500

    ...

--- modules/formbuilder/controllers/uiform-fb-controller-forms.php
+++ modules/formbuilder/controllers/uiform-fb-controller-forms.php
@@ -1196,19 +1196,46 @@ class Uiform_Fb_Controller_Forms extends Uiform_Base_Module {
         return $return;
     }
     
-            
+
+    /*
+* Search field on core data if exists
+*/    
+    private function isField_OnCoreData($core_data,$field_search)
+    {
+
+       foreach($core_data as $key => $field)
+               {
+                  if ( isset($core_data[$key][$field_search]) && !empty($core_data[$key][$field_search])){
+                         return $core_data[$key][$field_search];
+                  }
+               }
+               
+       return false;
+    }
+    /**
+     * Forms::generate_previewpanel_getField()
+     * 
+     * @return 
+     */
     protected function generate_previewpanel_getField($child_field) {
         $str_output = '';
         
         $data = array();
-        
+
         if(empty($this->current_data_form[intval($child_field['num_tab'])][$child_field['id']])){
-            $return = array();
-            $return['output_html'] = '';
-            return $return;
+            $tmp_data=$this->isField_OnCoreData($this->current_data_form,$child_field['id']);
+            if($tmp_data){
+                $data=$tmp_data;
+            }else{
+               $return = array();
+                $return['output_html'] = '';
+                return $return; 
+            }
+        }else{
+
+           $data = $this->current_data_form[intval($child_field['num_tab'])][$child_field['id']];    
         }
-        
-        $data = $this->current_data_form[intval($child_field['num_tab'])][$child_field['id']];
+
         $data['quick_options'] = self::render_template('formbuilder/views/fields/templates/prevpanel_quickopts.php', $data, 'always');
         switch (intval($child_field['type'])) {
             case 6:
